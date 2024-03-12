83ba04aab96b0445d4ee2e0205d4728dd3482b38
Adrien Cellario <adriencellario@gmail.com>
2024-03-12 17:03:15 +0100

    Create all Workflow - Model, Split, BatchDataset - TODO: Save Model & Metrics

--- ml_logic/main.py
+++ ml_logic/main.py
@@ -5,7 +5,8 @@ from model import (
     lossfn,
     callbacks_list,
 )
-from data_flow import create_dataset, batch_dataset, train_test_split
+import numpy as np
+from data_flow import create_dataset, batch_dataset
 from registry import save_model, save_results
 from params import *
 
@@ -13,10 +14,13 @@ from params import *
 print("==== Starting Workflow ====")
 
 # Step 1: Create the Dataset
-print("\n=== Step 1: Creating the Dataset ===")
-dataset = create_dataset(input="cloud")
+print("\n=== Step 1: Creating the Datasets ===")
+train_dataset, test_dataset = create_dataset(input="cloud")
 print("Dataset created successfully.")
-print(dataset)
+print(
+    f"Train/Test split created. Train size: {len(train_dataset)}, Test size: {len(test_dataset)}."
+)
+print(train_dataset, test_dataset)
 
 # Step 2: Initialize and Compile the Model
 print("\n=== Step 2: Initializing and Compiling the Model ===")
@@ -25,30 +29,33 @@ print("Model initialized and compiled successfully.")
 
 # Step 3: Batch the Dataset
 print("\n=== Step 3: Batching the Dataset ===")
-batched_dataset = batch_dataset(dataset, BATCH_SIZE)
+batched_train_dataset = batch_dataset(train_dataset, BATCH_SIZE)
+batched_test_dataset = batch_dataset(test_dataset, BATCH_SIZE)
 print(f"Dataset batched with batch size {BATCH_SIZE}.")
 
-# Step 4: Create Train/Test Split
-print("\n=== Step 4: Creating Train/Test Split ===")
-train, test = train_test_split(batched_dataset, 0.8)
-print(f"Train/Test split created. Train size: {len(train)}, Test size: {len(test)}.")
 
-# Step 5: Train the Model
-print("\n=== Step 5: Training the Model ===")
-history = train_model(model, train, test, epochs=EPOCHS, callbacks=callbacks_list)
+# Step 4: Train the Model
+print("\n=== Step 4: Training the Model ===")
+history = train_model(
+    model,
+    batched_train_dataset,
+    batched_test_dataset,
+    epochs=1,
+    callbacks=callbacks_list,
+)
 print("Model training complete.")
 
 val_recall = np.min(history.history["val_recall"])
 val_precision = np.min(history.history["val_precision"])
 val_accuracy = np.min(history.history["val_accuracy"])
 
-# Step 6: Save the Model
-print("\n=== Step 6: Saving the Model ===")
+# Step 5: Save the Model
+print("\n=== Step 5: Saving the Model ===")
 save_model(model)
 print("Model saved.")
 
-# Step 7: Save the metrics
-print("\n=== Step 7: Saving the metrics ===")
+# Step 6: Save the metrics
+print("\n=== Step 6: Saving the metrics ===")
 save_results(
     metrics=dict(recall=val_recall, precision=val_precision, accuracy=val_accuracy)
 )
